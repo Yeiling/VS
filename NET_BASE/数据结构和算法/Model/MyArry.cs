968e0429d78f14cd6228fbd69f310568f4b51f4b
HC <1415303215@qq.com>
2018-12-23 20:25:00 +0800

    修改接口

--- NET_BASE/数据结构和算法/Model/MyArry.cs
+++ NET_BASE/数据结构和算法/Model/MyArry.cs
@@ -53,6 +53,10 @@ namespace 数据结构和算法.Model
         #region 接口方法实现
         public int Length => Arrys.Length; //数组长度
         public int Count => Index; //数组元素个数
+        /// <summary>
+        /// 添加元素
+        /// </summary>
+        /// <param name="t">新元素</param>
         public void Add(object t)
         {
             if (Arrys.Length == Index)
@@ -60,7 +64,11 @@ namespace 数据结构和算法.Model
 
             Arrys[Index++] = t;
         }
-
+        /// <summary>
+        /// 制定位置添加元素
+        /// </summary>
+        /// <param name="index">索引位置</param>
+        /// <param name="t">新元素</param>
         public void Add(int index, object t)
         {
             if (Arrys.Length == Index)
@@ -88,12 +96,20 @@ namespace 数据结构和算法.Model
             }
             Index++;
         }
-
+        /// <summary>
+        /// 是否含有元素
+        /// </summary>
+        /// <param name="t">元素</param>
+        /// <returns>是否包含</returns>
         public bool Contains(object t)
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// 依据索引查找元素
+        /// </summary>
+        /// <param name="index">索引位置</param>
+        /// <returns>目标元素</returns>
         public object Get(int index)
         {
             if (index < 0 || index > Arrys.Length - 1)
@@ -101,12 +117,19 @@ namespace 数据结构和算法.Model
             else
                 return Arrys[index];
         }
-
+        /// <summary>
+        /// 元素的位置
+        /// </summary>
+        /// <param name="t">元素</param>
+        /// <returns>索引位置</returns>
         public int Indexof(object t)
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// 数组是否为空
+        /// </summary>
+        /// <returns>是否为空</returns>
         public bool IsEmpty()
         {
             if (Index >= 0)
@@ -114,17 +137,28 @@ namespace 数据结构和算法.Model
             else
                 return false;
         }
-
+        /// <summary>
+        /// 移除一个元素
+        /// </summary>
+        /// <param name="t">移除目标元素</param>
         public void Remove(object t)
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// 移除一个元素(依据索引)
+        /// </summary>
+        /// <param name="index">索引位置</param>
         public void Remove(int index)
         {
             Arrys[index] = null;
         }
-
+        /// <summary>
+        /// 替换元素
+        /// </summary>
+        /// <param name="index">索引位置</param>
+        /// <param name="newModel">新元素</param>
+        /// <returns>返回新元素</returns>
         public object Replace(int index, object newModel)
         {
             Arrys[index] = newModel;
