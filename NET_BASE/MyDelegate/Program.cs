3cb5fb1cf7e2d89f7d84ebb5e136b01f9e370e95
HC <1415303215@qq.com>
2019-03-03 10:41:27 +0800

    委托练习

--- NET_BASE/MyDelegate/Program.cs
+++ NET_BASE/MyDelegate/Program.cs
@@ -7,8 +7,8 @@ namespace MyDelegate
 {
     //委托放在命名空间中，和类是平级的
     //委托是方法的模版,类似于定义一个抽象方法，使用关键字delegate声明
-    public delegate string AddStr(string str1, string str2);    //有参数有返回值委托
-    public delegate void Test();   //无参数无返回值委托
+    public delegate string AddStr(string str1, string str2);//有参数返回值委托
+    public delegate void Test(); //无参数无返回值委托
 
     public delegate string changestr(string s);
     public delegate string[] changesz(string[] strs);
@@ -27,31 +27,46 @@ namespace MyDelegate
             //Console.WriteLine(str1);
 
             //使用委托 --- 实例1
-            string[] strs = new string[] { "sf", "ajoli", "tdj", "Wfsg", "dgd" };
-            strs = StrSZ(strs, chageStrs);   //使用委托方法 chageStrs
-            foreach (string s in strs)
-            {
-                Console.WriteLine(s);
-            }
-            strs = StrSZ(strs, otherchage);  //使用委托方法 otherchage
-            foreach (string s in strs)
-            {
-                Console.WriteLine(s);
-            }
-
-
+            //string[] strs = new string[] { "sf", "ajoli", "tdj", "Wfsg", "dgd" };
+            //strs = StrSZ(strs, chageStrs);   //使用委托方法 chageStrs
+            //foreach (string s in strs)
+            //{
+            //    Console.WriteLine(s);
+            //}
+            //strs = StrSZ(strs, otherchage);  //使用委托方法 otherchage
+            //foreach (string s in strs)
+            //{
+            //    Console.WriteLine(s);
+            //}
 
             //使用委托 --- 实例2
-            string[] sys = new string[] { "sf", "ajoli", "tdj", "Wfsg", "dgd" };
-            sys = CaiStr(sys, changecai);
-            foreach (string s in sys)
+            //string[] sys = new string[] { "sf", "ajoli", "tdj", "Wfsg", "dgd" };
+            //sys = CaiStr(sys, changecai);
+            //foreach (string s in sys)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            #region 委托---字符串操作练习
+            string[] strArry = new string[] { "2eGrty", "iHUHyb", "ni23sYG" };
+            StrArryHelper(strArry, str =>
             {
-                Console.WriteLine(s);
-            }
+                return str.ToLower();
+            });
+            string result1 = GetMax(strArry, (first, target) =>
+            {
+                return first.Length < target.Length;
+            });
+            int[] intArry = new int[] { 9, 2, 10, 22, 1, 5, 7, 3 };
+            int result2 = GetMax(intArry, (first, target) =>
+            {
+                return first < target;
+            });
+            #endregion
+
 
             Console.ReadKey();
         }
-
         //定义一个方法，改变数组中每一个元素
         #region 第一个实例
         public static string[] StrSZ(string[] strs, changestr change)
@@ -71,8 +86,6 @@ namespace MyDelegate
             return "***" + str + "***";
         }
         #endregion
-
-
         #region 第二个实例
         public static string[] CaiStr(string[] ss, changesz sz)
         {
@@ -87,16 +100,45 @@ namespace MyDelegate
             return sss;
         }
         #endregion
-
-        //定义委托实现方法
+        #region //定义委托实现方法
         public static string strcon(string s1, string s2)
         {
             return s1 + s2;
         }
+        #endregion
 
+        /// <summary>
+        /// 字符串数组操作函数练习
+        /// </summary>
+        /// <param name="strArry"></param>
+        /// <param name="strHandler"></param>
+        /// <returns></returns>
+        public static string[] StrArryHelper(string[] strArry, Func<string, string> strHandler)
+        {
+            for (int i = 0; i < strArry.Length; i++)
+            {
+                strArry[i] = strHandler(strArry[i]);
+            }
+            return strArry;
+        }
 
-
-
+        /// <summary>
+        /// 委托求数组最大值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="arry"></param>
+        /// <param name="predit"></param>
+        /// <returns></returns>
+        public static T GetMax<T>(T[] arry, Func<T, T, bool> predit)
+        {
+            T first = arry[0];
+            for (int i = 0; i < arry.Length; i++)
+            {
+                if (predit(first, arry[i]))
+                    first = arry[i];
+            }
+            return first;
+        }
 
     }
 }
