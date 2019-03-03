f63fe7eeded25e678b2d1666b50f64fa53bc418e
HC <1415303215@qq.com>
2019-03-03 11:44:57 +0800

    修改计算长度方法

--- NET_BASE/MyDelegate/Program.cs
+++ NET_BASE/MyDelegate/Program.cs
@@ -48,20 +48,24 @@ namespace MyDelegate
             //}
 
             #region 委托---字符串操作练习
-            string[] strArry = new string[] { "2eGwjrty", "iHUHyb", "ni23sYGwshih" };
+            string[] strArry = new string[] { "1q2w3D", "2A3s4X5v", "1R2d3y4K", "5B" };
             StrArryHelper(strArry, str =>
             {
                 return str.ToLower();
             });
-            string result1 = GetMaxOrMin(strArry, (first, target) =>
+
+            var result1 = GetMaxOrMin(strArry, (first, target) =>
             {
-                return first.Length < target.Length;
-            });
-            int[] intArry = new int[] { 9, 2, 10, 22, 1, 5, 7, 3 };
-            int result2 = GetMaxOrMin(intArry, (first, target) =>
+                //return target.Length - first.Length; //求长度最大值
+                return first.Length - target.Length; //求长度最小值
+            }, out string maxOrMinLen);
+
+            int[] intArry = new int[] { 9, 2, 10, 22, 1, 5, 0, 7, 3, 1, 1, 22, 5, 22, -4 };
+            var result2 = GetMaxOrMin(intArry, (first, target) =>
             {
-                return first > target;
-            });
+                //return target - first; //求最大值
+                return first - target; //求最小值
+            }, out int maxOrMin);
             #endregion
 
 
@@ -125,9 +129,9 @@ namespace MyDelegate
         /// <summary>
         /// 委托求数组最大值
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="arry"></param>
-        /// <param name="predit"></param>
+        /// <typeparam name="T">泛型类型</typeparam>
+        /// <param name="arry">数组</param>
+        /// <param name="predit">比较委托函数</param>
         /// <returns></returns>
         public static T GetMaxOrMin<T>(T[] arry, Func<T, T, bool> predit)
         {
@@ -138,6 +142,38 @@ namespace MyDelegate
                     first = arry[i];
             }
             return first;
+        }
+        /// <summary>
+        /// 求数组最大值，并统计个数
+        /// </summary>
+        /// <typeparam name="T">泛型类型</typeparam>
+        /// <param name="arry">数组</param>
+        /// <param name="predit">比较委托函数</param>
+        /// <param name="totalNum">计数</param>
+        /// <returns></returns>
+        public static Dictionary<T, int> GetMaxOrMin<T>(T[] arry, Func<T, T, int> predit, out T maxOrMin)
+        {
+            Dictionary<T, int> result = new Dictionary<T, int>();
+            maxOrMin = arry[0];
+            for (int i = 0; i < arry.Length; i++)
+            {
+                if (predit(maxOrMin, arry[i]) >= 0)
+                {
+                    maxOrMin = arry[i];
+                    if (!result.Keys.Contains(maxOrMin))
+                        result.Add(maxOrMin, 1);
+                    else
+                        result[maxOrMin]++;
+                }
+                else if (predit(maxOrMin, arry[i]) < 0)
+                {
+                    if (!result.Keys.Contains(arry[i]))
+                        result.Add(arry[i], 1);
+                    else
+                        result[arry[i]]++;
+                }
+            }
+            return result;
         }
 
     }
