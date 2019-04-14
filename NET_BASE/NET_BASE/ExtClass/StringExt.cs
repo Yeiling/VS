da5b054687e2db134edce5ac50f60cb6a9865f4c
HC <1415303215@qq.com>
2019-04-14 12:41:00 +0800

    修改提交

--- NET_BASE/NET_BASE/ExtClass/StringExt.cs
+++ NET_BASE/NET_BASE/ExtClass/StringExt.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 
 namespace NET_BASE.ExtClass
 {
@@ -46,5 +43,6 @@ namespace NET_BASE.ExtClass
             }
             return chr.ToString();
         }
+
     }
 }
