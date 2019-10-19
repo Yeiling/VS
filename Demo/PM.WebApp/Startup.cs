941e5aa5077389f72b8f855bc05a571b0d6b62a4
Yeiling <2219572927@qq.com>
2019-10-19 08:22:58 +0800

    添加IOC

--- Demo/PM.WebApp/Startup.cs
+++ Demo/PM.WebApp/Startup.cs
@@ -33,6 +33,9 @@ namespace PM.WebApp
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.UseJsonConfig(Configuration);
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
