18f3a566915743cce54205a23e5a8fff1444dd04
CaiHui <2219572927@qq.com>
2019-11-01 16:15:34 +0800

    xiugai

--- ShuiYinTong.API/shuiyintong.Api/AutoFacAop/LogInterceptor.cs
+++ ShuiYinTong.API/shuiyintong.Api/AutoFacAop/LogInterceptor.cs
@@ -1,5 +1,6 @@
 ﻿using Castle.DynamicProxy;
 using log4net;
+using System.Linq;
 
 namespace shuiyintong.Api.AutoFacAop
 {
@@ -19,17 +20,15 @@ namespace shuiyintong.Api.AutoFacAop
         /// <param name="invocation"></param>
         public void Intercept(IInvocation invocation)
         {
-            //string str = string.Format("方法执行前:拦截{0}类下的方法{1}的参数是{2}",
-            //    invocation.TargetType.FullName,
-            //    invocation.Method.Name, string.Join(",", invocation.Arguments.Select(a => (a ?? "").ToString()).ToArray()));
+            string str = string.Format("方法执行前:拦截{0}类下的方法{1}的参数是{2}",
+                invocation.TargetType.FullName,
+                invocation.Method.Name, string.Join(",", invocation.Arguments.Select(a => (a ?? "").ToString()).ToArray()));
 
             //在被拦截的方法执行完毕后 继续执行
             //调用Proceed函数将会实现方法体以外的函数，就是切面以外的函数
-            //invocation.Proceed();
+            invocation.Proceed();
 
-            //str = string.Format("方法执行完毕，返回结果：{0}", invocation.ReturnValue);
-
-            
+            str = string.Format("方法执行完毕，返回结果：{0}", invocation.ReturnValue);           
 
         }
     }
