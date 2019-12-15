cdad969b947ecf4ab14509b9a87d55773810cd8c
InSpire <InSpire@INSPIRE-PC>
2019-12-15 10:08:33 +0800

    12

---
+++ NET_BASE/Java分布式算法/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Java分布式算法
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
